c7d3733bcb29987f87fc46a7627df3b6eae5afc2
mohammeddib10 <mohammeddib85@gmail.com>
2020-05-01 18:18:29 +0200

    first commit

---
+++ catkin_ws/build/follow_me/CMakeFiles/moving_person_detector_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/moving_person_detector_node.dir/src/moving_person_detector_node.cpp.o"
+  "/home/mohammeddib/catkin_ws/devel/lib/follow_me/moving_person_detector_node.pdb"
+  "/home/mohammeddib/catkin_ws/devel/lib/follow_me/moving_person_detector_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/moving_person_detector_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
